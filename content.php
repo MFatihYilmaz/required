fe994134aa637919d8e650b7f9a13c9b7dbfc653
M Fatih YILMAZ <mfth78@hotmail.com>
2023-08-23 13:28:11 +0300

    fixed

--- content.php
+++ content.php
@@ -11,25 +11,20 @@
     <form action="content.php" method="get">
         <h3>Get file</h3>
         <select name="file" id="">
-            <option value="files/secret">Secret File</option>
-            <option value="files/system">System File</option>
-            <option value="files/settings">Settings File</option>
-            <option value="files/info">info File</option>
+            <option value="secret">Secret File</option>
+            <option value="system">System File</option>
+            <option value="settings">Settings File</option>
+            <option value="info">info File</option>
         </select>
     <button type="submit">Submit</button>
     </form>    
 
 <div>
 <?php
-
-if(isset($_GET["ext"]) && isset($_GET["file"])){
-    include("./".$_GET["file"].$_GET["ext"]);
-}elseif(isset($_GET["file"])){
-    include("./".$_GET["file"].".php");
-}else{
- include("index.php");   
+$ext=(isset($_GET["ext"]))?$_GET["ext"]:".php";
+if(isset($_GET["file"])){
+    include("./".$_GET["file"].$ext);
 }
-
 
 ?>
 
